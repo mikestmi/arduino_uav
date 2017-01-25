67b2240abaae22b3fb4a66619b9a06394518983b
Michael <michaelv@windowslive.com>
2017-01-25 17:42:30 +0200

    Add DHT11 sensor to ground station 1 to take a measurement when a message from uav device is received

--- ground_station1/ground_station1.ino
+++ ground_station1/ground_station1.ino
@@ -9,13 +9,18 @@ RF24Network network(radio);
 const uint16_t ground_station_1 = 011;
 const uint16_t uav = 01;
 
+#define DHT11_PIN 3
+dht DHT;
+
+String temperature;
+String humidity;
+
 void setup() {
   SPI.begin();
   radio.begin();
   radio.setDataRate(RF24_250KBPS);
   radio.setPALevel(RF24_PA_MAX);
   network.begin(108, ground_station_1);
-
 }
 
 void loop() {
@@ -25,5 +30,10 @@ void loop() {
     RF24NetworkHeader header;
     char c;
     network.read(header, &c, sizeof(c));
+
+    DHT.read11(DHT11_PIN);
+    humidity = String(DHT.humidity, 0);
+    temperature = String(DHT.temperature, 0);
+    String Entry = temperature + "C," + humidity + "%";
   }
 }
