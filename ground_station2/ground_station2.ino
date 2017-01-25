9340be7931392928fc4db4adc3e01bdc9f64ab4f
Michael <michaelv@windowslive.com>
2017-01-25 18:59:25 +0200

    Add relay module to ground station 2, to control an electric motor depending on the sensor measurements

--- ground_station2/ground_station2.ino
+++ ground_station2/ground_station2.ino
@@ -9,8 +9,10 @@ const uint16_t ground_station_2 = 021;
 const uint16_t uav = 01;
 
 int moistPin = A0;
+int relayPin = 7;
 int moistValue = 0;
 String moistPerc;
+String Status;
 
 int convertToPercent(int value) {
   int percentValue = 0;
@@ -24,7 +26,7 @@ void setup() {
   radio.setDataRate(RF24_250KBPS);
   radio.setPALevel(RF24_PA_MAX);
   network.begin(108, ground_station_2);
-
+  pinMode(relayPin, OUTPUT);
 }
 
 void loop() {
@@ -32,6 +34,15 @@ void loop() {
 
   moistValue = analogRead(moistPin);
   moistPerc = convertToPercent(moistValue);
+
+  if (convertToPercent(moistValue) < 30) {
+    digitalWrite(relayPin, HIGH);
+    Status = "OFF";
+  }
+  else {
+    digitalWrite(relayPin, LOW);
+    Status = "ON";
+  }
 
   while ( network.available() ) {
     RF24NetworkHeader header;
